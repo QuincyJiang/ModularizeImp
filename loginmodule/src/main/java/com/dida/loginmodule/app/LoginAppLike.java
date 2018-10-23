7bcd30f8dc371549ab14c1c8a2ee44837210efdc
QuincyJiang <Jiangxiaqiu@yy.com>
2018-10-23 21:52:20 +0800

    fix build exception

--- loginmodule/src/main/java/com/dida/loginmodule/app/LoginAppLike.java
+++ loginmodule/src/main/java/com/dida/loginmodule/app/LoginAppLike.java
@@ -12,7 +12,12 @@ import com.dida.commonservicelib.utils.AppUtils;
 import com.dida.loginmodule.config.Config;
 
 import cn.bmob.v3.Bmob;
-
+/**
+ * Created by QuincyJiang at 2018/10/23 .
+ * Description:
+ * 正式环境会被壳app的Application回调 在这里初始化一下Bmob
+ * 目前回调接口还是用的反射查找的方式，后期会使用编译期注解来动态创建代码
+ */
 public class LoginAppLike implements ApplicationLike {
     @Override
     public void onCreate() {
